d51253ca28e87e42207f3a38d55932e14bc813c3
LucasAmmer <lucasammerlaan123@gmail.com>
2023-10-10 21:02:39 +0200

    added verbose and json parsing

--- main.cpp
+++ main.cpp
@@ -12,6 +12,15 @@ struct config
 
 int main(int argc, char *argv[])
 {
+    bool verbose = false;
+    for (int i = 0; i < argc; i++)
+    {
+        if (argv[i] == "-v" || argv[i] == "--verbose")
+        {
+            verbose = true;
+        }
+    }
+
     if (argc < 2)
     {
         std::cout << "Usage: " << argv[0] << " <instruction>" << std::endl
@@ -24,12 +33,15 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    json configuration;
+
     if (!std::filesystem::exists("./.config"))
     {
         std::ofstream configFile("./.config");
         json defaultConfig;
         defaultConfig["listFile"] = "./.list";
         configFile << defaultConfig;
+        configuration = defaultConfig;
         configFile.close();
         std::ofstream listFile("./.list");
         listFile << "";
@@ -38,17 +50,22 @@ int main(int argc, char *argv[])
     }
     else
     {
-        std::cout << "found .config file" << std::endl;
+        if (verbose)
+            std::cout << "found .config file" << std::endl;
         std::string configContent;
         std::ifstream configFile("./.config");
 
         while (std::getline(configFile, configContent))
         {
-            std::cout << configContent;
+            std::cout << configContent << std::endl;
+            configuration = json::parse(configContent);
         }
 
         configFile.close();
     }
+
+    if (verbose)
+        std::cout << "list file name: " << configuration["listFile"] << std::endl;
 
     return 0;
 }
